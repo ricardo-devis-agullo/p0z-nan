db1af9848a3c82906fa79ee57551b78a00c73a40
Ricardo Devis Agullo <mkredz@gmail.com>
2024-04-19 09:29:16 +0200

    db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,13 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const User = defineTable({
+  columns: {
+    name: column.text({ primaryKey: true }),
+    password: column.text(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-	tables: {},
+  tables: { User },
 });
